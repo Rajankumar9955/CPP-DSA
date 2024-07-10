0c59ced4056c7942e5fc79ffdd4d6944966a9aeb
Rajan <krajan92946@gmail.com>
2024-07-10 11:50:29 +0530

    heap sort

---
+++ HeapSort/heapSort.cpp
@@ -0,0 +1,58 @@
+/*
+some formula:- parent=n/2;  
+               left:=2*i;
+               right:=2*i+1
+
+
+*/
+
+
+#include<iostream>
+using namespace std;
+class heaps{
+    int arr[10];
+    int s,index;
+    public:heaps()
+    {
+        s=0;
+    }
+    void maxhp(int d)
+    {
+        s=s+1;
+        index=s;
+        arr[index]=d;
+        while(index>1)
+        {
+            int parent=index/2;
+            if(arr[parent]<arr[index])
+            {
+                swap(arr[index],arr[parent]);
+                index=parent;
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+    void show()
+    {
+        for(int i=1; i<=s; ++i)
+        {
+            cout<<arr[i]<<"\n";
+        }
+    }
+};
+int main()
+{
+    heaps h;
+    h.maxhp(50);
+     h.maxhp(20);
+      h.maxhp(5);
+       h.maxhp(100);
+        h.maxhp(10);
+         h.maxhp(30);
+          h.maxhp(8);
+           h.maxhp(1);
+           h.show();
+}
