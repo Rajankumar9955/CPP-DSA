628e2bcde4e58c325e5d24f320bf5c7187851104
Rajan <krajan92946@gmail.com>
2024-08-30 00:39:20 +0530

    vecotor practice:

--- Vector/Vector questions/Q2.cpp
+++ Vector/Vector questions/Q2.cpp
@@ -27,3 +27,19 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+int main()
+{
+     vector<int>v1{2,3,4,2};
+     vector<int>v2{4,8};
+     int s1,s2;
+     s1=s2=0;
+     for(int i=0; i<v1.size(); ++i)
+     {
+        s1=s1*10+v1[i];
+     }
+     for(int j=0; j<v2.size(); ++j)
+     {
+        s2=s2*10+v2[j];
+     }
+     cout<<s1+s2;
+}
--- Vector/Vector questions/Target.cpp
+++ Vector/Vector questions/Target.cpp
@@ -1,31 +1,85 @@
+// #include<iostream>
+// #include<algorithm>
+// #include<vector>
+// using namespace std;
+// int main()
+// {
+//    vector<int>v{9,3,2,5,1};
+//    sort(v.begin(),v.end());
+//    int target=8;
+//    int l=0;
+//    int r=v.size()-1;
+//    int sum=0;
+//    while(l<r)
+//    {
+//     sum=v[l]+v[r];
+//     if(sum==target)
+//     {
+//         cout<<"True";
+//         break;
+//     }
+//     else if(sum<target)
+//     {
+//         l++;
+//     }
+//     else if(sum>target)
+//     {
+//         r--;
+//     }
+//    }
+// }
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
 using namespace std;
 int main()
 {
-   vector<int>v{9,3,2,5,1};
-   sort(v.begin(),v.end());
-   int target=8;
-   int l=0;
-   int r=v.size()-1;
-   int sum=0;
-   while(l<r)
-   {
-    sum=v[l]+v[r];
-    if(sum==target)
+    vector<int>v{1,2,3,6,4,8,9};
+    sort(v.begin(),v.end());
+    cout<<"Enter the target Elements:";
+    int target;
+    cin>>target;
+    int l=0; 
+    int r=v.size()-1;
+    int sum=0;
+    while(l<r)
     {
-        cout<<"True";
-        // cout<<l<<r;
-        break;
+        sum=v[l]+v[r];
+        if(sum==target)
+        {
+            cout<<"Found";
+            break;
+        }
+        else if(sum<target)
+        {
+            l++;
+        }
+        else if(sum>target)
+        {
+            r--;
+        }
     }
-    else if(sum<target)
-    {
-        l++;
-    }
-    else if(sum>target)
-    {
-        r--;
-    }
-   }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
