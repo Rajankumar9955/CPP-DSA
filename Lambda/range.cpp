81be7b31087384e90990eb26f147f4933b370101
Rajan <krajan92946@gmail.com>
2024-10-31 23:59:10 +0530

    Range in Lambda

--- Lambda/range.cpp
+++ Lambda/range.cpp
@@ -11,6 +11,7 @@ int main()
      //if any one will fail then result will be 1;
      cout<<any_of(v.begin(),v.end(),[](int a){return a>40;});
 
+
       //if none will pass then result will 1
       cout<<none_of(v.begin(),v.end(),[](int a){return a>100;});
 }
