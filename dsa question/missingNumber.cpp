0ccde3718fb061c9121285e48b2f1bedff929e51
Rajan <krajan92946@gmail.com>
2024-11-30 22:45:58 +0530

    missing number:

--- dsa question/missingNumber.cpp
+++ dsa question/missingNumber.cpp
@@ -5,26 +5,18 @@
 using namespace std;
 int main()
 {
-//     array<int,4>a={0,2,3,4};  //if we start from 0 the include nothing but if start from 1 then include +1
-//     int n=a.size();
-//     int sum=n*(n+1)/2;
-//     int s=0;
-//     for(int i=0; i<a.size(); ++i){
-//         s+=a[i];
-//     }
-//    cout<<sum-s;
+    array<int,4>a={0,2,3,4};  //if we start from 0 the include nothing but if start from 1 then include +1
+    int n=a.size();
+    int sum=n*(n+1)/2;
+    int s=0;
+    for(int i=0; i<a.size(); ++i){
+        s+=a[i];
+    }
+   cout<<sum-s;
 
 
 
-array<int,6>a={0,1,3,4,5,6};
-int n=a.size();
-int sum=n*(n+1)/2;
-int s=0;
-for(int i=0; i<a.size(); ++i)
-{
-    s+=a[i];
-}
-cout<<sum-s;
+
 }
 
 
