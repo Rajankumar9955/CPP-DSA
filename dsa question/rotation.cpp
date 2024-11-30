5cf25eb2a0d9b1b765092d6a58a193f21344c669
Rajan <krajan92946@gmail.com>
2024-11-30 22:47:02 +0530

    rotation number

--- dsa question/rotation.cpp
+++ dsa question/rotation.cpp
@@ -22,28 +22,28 @@
 //============================Dymanically program=======================
 
 
-// #include<iostream>
-// #include<array>
-// using namespace std;
-// int main()
-// {
-//     cout<<"Your Array like That:[1,2,3,4,5]";
-//     cout<<"\nEnter Your Step:";
-//     int st;
-//     cin>>st;
-//     array<int,5>a={1,2,3,4,5};
-//     array<int,5>temp;
-//     int n=a.size();
-//     if(st<=n)
-//     {
-//         for(int i=0; i<a.size(); ++i)
-//         {
-//             temp[(i+st)%n]=a[i];
-//         }
-//         for(auto p:temp)
-//         {
-//             cout<<p<<"\t";
-//         }
-//     }
-// }
+#include<iostream>
+#include<array>
+using namespace std;
+int main()
+{
+    cout<<"Your Array like That:[1,2,3,4,5]";
+    cout<<"\nEnter Your Step:";
+    int st;
+    cin>>st;
+    array<int,5>a={1,2,3,4,5};
+    array<int,5>temp;
+    int n=a.size();
+    if(st<=n)
+    {
+        for(int i=0; i<a.size(); ++i)
+        {
+            temp[(i+st)%n]=a[i];
+        }
+        for(auto p:temp)
+        {
+            cout<<p<<"\t";
+        }
+    }
+}
 
