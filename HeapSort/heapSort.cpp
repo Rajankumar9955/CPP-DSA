4366acf1ce8db9abfa43003dcecb7171177b1063
Rajan <krajan92946@gmail.com>
2025-01-13 16:03:46 +0530

    data added

--- HeapSort/heapSort.cpp
+++ HeapSort/heapSort.cpp
@@ -7,64 +7,12 @@ some formula:- parent=n/2;
 */
 
 
-// #include<iostream>
-// using namespace std;
-// class heaps{
-//     int arr[10];
-//     int s,index;
-//     public:heaps()
-//     {
-//         s=0;
-//     }
-//     void maxhp(int d)
-//     {
-//         s=s+1;
-//         index=s;
-//         arr[index]=d;
-//         while(index>1)
-//         {
-//             int parent=index/2;
-//             if(arr[parent]<arr[index])
-//             {
-//                 swap(arr[index],arr[parent]);
-//                 index=parent;
-//             }
-//             else
-//             {
-//                 return;
-//             }
-//         }
-//     }
-//     void show()
-//     {
-//         for(int i=1; i<=s; ++i)
-//         {
-//             cout<<arr[i]<<"\n";
-//         }
-//     }
-// };
-// int main()
-// {
-//     heaps h;
-//     h.maxhp(50);
-//      h.maxhp(20);
-//       h.maxhp(5);
-//        h.maxhp(100);
-//         h.maxhp(10);
-//          h.maxhp(30);
-//           h.maxhp(8);
-//            h.maxhp(1);
-//            h.show();
-// }
-
-
 #include<iostream>
 using namespace std;
 class heaps{
     int arr[10];
     int s,index;
-    public:
-    heaps()
+    public:heaps()
     {
         s=0;
     }
@@ -97,8 +45,7 @@ class heaps{
 };
 int main()
 {
-  heaps h;
-
+    heaps h;
     h.maxhp(50);
      h.maxhp(20);
       h.maxhp(5);
@@ -108,5 +55,7 @@ int main()
           h.maxhp(8);
            h.maxhp(1);
            h.show();
-
 }
+
+
+
--- dsa question/missingNumber.cpp
+++ dsa question/missingNumber.cpp
@@ -1,22 +1,37 @@
-//missing number
+// //missing number
+
+// #include<iostream>
+// #include<array>
+// using namespace std;
+// int main()
+// {
+//     array<int,4>a={0,2,3,4};  //if we start from 0 the include nothing but if start from 1 then include +1
+//     int n=a.size();
+//     int sum=n*(n+1)/2;
+//     int s=0;
+//     for(int i=0; i<a.size(); ++i){
+//         s+=a[i];
+//     }
+//    cout<<sum-s;
+
+
+
+
+// }
+
 
 #include<iostream>
-#include<array>
 using namespace std;
+#include<array>
 int main()
 {
-    array<int,4>a={0,2,3,4};  //if we start from 0 the include nothing but if start from 1 then include +1
-    int n=a.size();
+    array<int,4>a={1,3,4,5};
+    int n=a.size()+1;
     int sum=n*(n+1)/2;
     int s=0;
-    for(int i=0; i<a.size(); ++i){
+    for(int i=0; i<a.size(); i++)
+    {
         s+=a[i];
     }
-   cout<<sum-s;
-
-
-
-
+    cout<<sum-s;
 }
-
-
